14ad3dafb198f94951f6731ee9dd88b83ba615a2
aaron9589 <aaron@cybus.net>
2024-05-18 22:48:23 +1000

    added sleep for db issue

--- start.sh
+++ start.sh
@@ -5,8 +5,10 @@ if [ "$PROVISION_DATABASE" = "1" ]; then
     # Check if the database has been initialized
     if [ ! -f /opt/sql.initialized ]; then
 
+        # Sleeping as MariaDB takes a bit to get going
+        sleep 20
         # Execute the SQL script on the newly created database with authentication
-        mysql -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE < /var/www/html/sts/create_sts_db3.sql && \
+        mysql -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE < /var/www/html/sts/create_sts_db3.sql
 
         # Create a marker to indicate that database initialization is complete
         touch /opt/sql.initialized
