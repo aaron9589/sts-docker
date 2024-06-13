8c5cd23cbece9192defd04d290effd02d0dc51f5
aaron9589 <aaron@cybus.net>
2024-06-13 20:25:28 +1000

    path update

--- phpqrcode/qrlib.php
+++ phpqrcode/qrlib.php
@@ -25,8 +25,8 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  */
 	
-	$QR_BASEDIR = dirname(__FILE__).DIRECTORY_SEPARATOR;
-	
+	//$QR_BASEDIR = dirname(__FILE__).DIRECTORY_SEPARATOR;
+	$QR_BASEDIR = '../phpqrcode/';
 	// Required libs
 	
 	include $QR_BASEDIR."qrconst.php";
