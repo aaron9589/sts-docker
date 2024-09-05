7e92a2dbd06920f62997074ba6d5791668447773
aaron-s <aaron@cybus.net>
2024-09-05 18:21:02 +1000

    Add files via upload

---
+++ sts/manage-cargo.php
@@ -0,0 +1,44 @@
+<?php
+$cargo_file = 'cargo_list.txt';
+
+function get_cargo() {
+    global $cargo_file;
+    return file($cargo_file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+}
+
+function remove_cargo($cargo) {
+    global $cargo_file;
+    $cargo_list = get_cargo();
+    $cargo_list = array_diff($cargo_list, [$cargo]);
+    file_put_contents($cargo_file, implode("\n", $cargo_list));
+}
+
+function reset_cargo() {
+    global $cargo_file;
+    $original_cargo = [
+        'Coal', 'Grain', 'Timber', 'Oil', 'Ore', 
+        'Containers', 'Automobiles', 'Livestock'
+    ];
+    file_put_contents($cargo_file, implode("\n", $original_cargo));
+}
+
+$action = $_GET['action'] ?? '';
+
+switch ($action) {
+    case 'get':
+        echo json_encode(get_cargo());
+        break;
+    case 'remove':
+        $cargo = $_GET['cargo'] ?? '';
+        if ($cargo) {
+            remove_cargo($cargo);
+        }
+        echo json_encode(['success' => true]);
+        break;
+    case 'reset':
+        reset_cargo();
+        echo json_encode(['success' => true]);
+        break;
+    default:
+        echo json_encode(['error' => 'Invalid action']);
+}
